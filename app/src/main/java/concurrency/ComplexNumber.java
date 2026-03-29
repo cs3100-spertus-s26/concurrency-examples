d155b782cd4a327b621aea68f5280c7be09d655e
espertusnu <espertusnu@users.noreply.github.com>
2026-03-29 10:50:32 -0700

    Add single-threaded complex numbers

---
+++ app/src/main/java/concurrency/ComplexNumber.java
@@ -0,0 +1,32 @@
+package concurrency;
+
+import static concurrency.Utilities.printThread;
+
+public class ComplexNumber {
+  private int real;
+  private int imaginary;
+
+  public ComplexNumber(int real, int imaginary) {
+    this.real = real;
+    this.imaginary = imaginary;
+  }
+
+  public void negate() {
+    printThread("At start of negate()");
+    real = -real;
+    imaginary = -imaginary;
+    printThread("At end of negate()");
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%d + %di", real, imaginary);
+  }
+
+  public static void main(String[] args) {
+      ComplexNumber c = new ComplexNumber(1, 1);
+      printThread("Original: " + c);
+      c.negate();
+      printThread("Negated: " + c);
+  }
+}
