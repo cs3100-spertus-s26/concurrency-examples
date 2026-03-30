2f709f208ded04ed9b0109d37bd4aa85516aa0b3
espertusnu <espertusnu@users.noreply.github.com>
2026-03-29 20:07:18 -0700

    Add plant-watering example

---
+++ app/src/main/java/concurrency/Plant.java
@@ -0,0 +1,42 @@
+package concurrency;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static concurrency.Utilities.printThread;
+
+public class Plant implements Runnable {
+
+  private static final int WATERING_INTERVAL_DAYS = 3;
+  private Instant lastTimeWatered;
+  private int numTimesWatered = 0;
+
+  private int daysSinceWatered() {
+    return (int) Duration.between(lastTimeWatered,
+        Instant.now()).toDays();
+  }
+
+  private void waterPlant() {
+    printThread("Watering plant.");
+    numTimesWatered++;
+    lastTimeWatered = Instant.now();
+  }
+
+  @Override
+  public void run() {
+    if (lastTimeWatered == null
+        || daysSinceWatered() >= WATERING_INTERVAL_DAYS) {
+      printThread("About to water plant.");
+      waterPlant();
+      printThread("numTimesWatered: " + numTimesWatered);
+    } else {
+      printThread("No need to water plant.");
+    }
+  }
+
+  public static void main(String[] args) {
+    Plant plant = new Plant();
+    new Thread(plant).start(); // roommate 1
+    new Thread(plant).start(); // roommate 2
+  }
+}
