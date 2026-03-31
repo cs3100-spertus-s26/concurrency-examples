ef9ef9f8796618fb42ac967614f4e8282daa4008
espertusnu <espertusnu@users.noreply.github.com>
2026-03-31 14:45:46 -0700

    Add CookMyBooks example

---
+++ app/src/main/java/concurrency/CookMyBooks.java
@@ -0,0 +1,39 @@
+package concurrency;
+
+public class CookMyBooks {
+  private static final long START_TIME = System.currentTimeMillis();
+
+  private static void log(String message) {
+    long elapsed = System.currentTimeMillis() - START_TIME;
+    String thread = Thread.currentThread().getName();
+    System.out.printf("%4dms %s %s%n", elapsed, thread, message);
+  }
+
+  private static String fetchRecipe() {
+    log("Fetching recipe...");
+    try {
+      Thread.sleep(2000); // simulates slow network call
+    } catch (InterruptedException e) {
+      // this can't happen in our program
+    }
+    log("Got recipe!");
+    return "Cookie Recipe";
+  }
+
+  private static void updateUI(String recipe) {
+    log("Updating UI: " + recipe);
+  }
+
+  // This runs in the UI thread.
+  private static void handleFetchRecipeRequest()  {
+    log("UI thread received fetch recipe request");
+    log("UI is non-responsive");
+    String recipe = fetchRecipe();
+    updateUI(recipe);
+    log("UI is now responsive again");
+  }
+
+  public static void main(String[] args) {
+    handleFetchRecipeRequest();
+  }
+}
